50d99655db0190ac0bbe953828833ae297c600c6
Sofi Brugo <110871256+Sbrugo@users.noreply.github.com>
2025-03-05 16:05:43 -0300

    Update UserRepository.java

--- src/main/java/com/javacrudrestapi/demo/repository/UserRepository.java
+++ src/main/java/com/javacrudrestapi/demo/repository/UserRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT * FROM user_inventory WHERE username = ?1", nativeQuery = true)
-    List<User> getUsername(String username);
+    List<User> findUsersByUsername(String username);
 }
