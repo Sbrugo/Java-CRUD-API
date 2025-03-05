229f5584167d1bf3c84dfb7a6268e6bda66758d7
Sofi Brugo <110871256+Sbrugo@users.noreply.github.com>
2025-03-05 16:04:40 -0300

    Update UserRepository.java
    Add user repository file

--- src/main/java/com/javacrudrestapi/demo/repository/UserRepository.java
+++ src/main/java/com/javacrudrestapi/demo/repository/UserRepository.java
@@ -1,1 +1,14 @@
+package com.javacrudrestapi.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.javacrudrestapi.demo.model.User;
+import org.springframework.data.jpa.repository.Query;
+
+
+import java.util.List;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "SELECT * FROM user_inventory WHERE username = ?1", nativeQuery = true)
+    List<User> getUsername(String username);
+}
