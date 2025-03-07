7a72031cc41b8c33f8617506eff35111367272e8
Sofi Brugo <110871256+Sbrugo@users.noreply.github.com>
2025-03-07 16:02:23 -0300

    Update User.java

--- src/main/java/com/javacrudrestapi/demo/model/User.java
+++ src/main/java/com/javacrudrestapi/demo/model/User.java
@@ -15,8 +15,9 @@ public class User {
     private Long id;
 
     @NotBlank(message = "Name is required")
-    @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters")
-    private String name;
+    @Column(unique = true)
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
