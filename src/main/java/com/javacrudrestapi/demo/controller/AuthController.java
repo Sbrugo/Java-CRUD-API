de70daae651635559cf6508461c0f55321375326
Sofi Brugo <110871256+Sbrugo@users.noreply.github.com>
2025-03-07 12:17:39 -0300

    Create AuthController.java

---
+++ src/main/java/com/javacrudrestapi/demo/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.javacrudrestapi.demo.controller;
+
+import com.javacrudrestapi.demo.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> loginRequest) {
+        String email = loginRequest.get("email");
+        String password = loginRequest.get("password");
+        String token = authService.login(email, password);
+
+        return ResponseEntity.ok(Map.of("token", token));
+    }
+}
